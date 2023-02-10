9a9dcbb22d970fc4bfc473c9754ef54cc557f3d2
Empirey10 <genzyy04@gmail.com>
2023-02-10 14:26:29 -0800

    add file

---
+++ ProgramJavaPertama.java
@@ -0,0 +1,12 @@
+  public class ProgramJavaPertama
+  {
+    /* Ini adalah program java pertama.
+     * Ini akan menampilkan output 'Halo Dunia'  
+     */
+ 
+  public static void main(String[]args)
+  {
+    System.out.println("Halo Dunia");
+    // menampilkan Halo Dunia
+  }
+}
